c0e325bf41ad39fd3c9e7cf75020ab99f2dba5f6
alelarge <armelle.lelarge@shodo.io>
2023-11-15 22:51:31 +0100

    refactor must return all positions of the neighbours at the center of the grid

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,16 +1,10 @@
 class Grid(val rows: Int, val cols: Int) {
     fun getNeighbourPositions(position: Position, scale: Int): Set<Position> {
-        val neighbourPositions = mutableSetOf<Position>()
-        for (i in -1 * scale..scale) {
-            for (j in -1 * scale..scale) {
-                if (i == 0 && j == 0) continue
-                val newX = position.x + i
-                val newY = position.y + j
-                if (newX in 0 until rows && newY in 0 until cols) {
-                    neighbourPositions.add(Position(newX, newY))
-                }
+        return setOf(-scale to -scale, 0 to -scale, scale to -scale, -scale to 0, scale to 0, -scale to scale, 0 to scale, scale to scale)
+            .map { (dx, dy) ->
+                val (x, y) = position
+                Position(position.x + dx, position.y + dy)
             }
-        }
-        return neighbourPositions
+            .filter { (x,y) -> x>=0 && y>=0 && x<rows && y<cols}.toSet()
     }
 }
