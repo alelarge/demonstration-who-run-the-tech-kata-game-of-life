80fe06dd005a18340c433b455b81191e67497f2c
alelarge <armelle.lelarge@shodo.io>
2023-11-15 23:02:48 +0100

    test failed cell should be alive when initialized as alive

---
+++ src/test/kotlin/CellTest.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CellTest {
+    @Test
+    fun `cell should be alive when initialized as alive`(){
+        val aliveCell = Cell(CellState.ALIVE)
+        assertThat(aliveCell.isAlive()).isTrue
+    }
+}
