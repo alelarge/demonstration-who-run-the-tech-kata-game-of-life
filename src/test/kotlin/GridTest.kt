70c2f81831b1f2b8595ebd94237519a2810dde03
alelarge <armelle.lelarge@shodo.io>
2023-11-15 21:34:02 +0100

    assertions initialized grid 3x5

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -1,8 +1,11 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+
 
 class GridTest {
     @Test
     fun `initialized a 3x5 grid`(){
-
+        assertThat(grid.rows).isEqualTo(3)
+        assertThat(grid.cols).isEqualTo(5)
     }
 }
