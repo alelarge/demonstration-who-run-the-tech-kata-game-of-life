a2339fcc78434cef9c97df40e8fd8dc45e2f0db5
alelarge <armelle.lelarge@shodo.io>
2023-11-15 22:30:10 +0100

    assertion fail to do position must return all positions of the neighbours at the center of the grid

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -10,12 +11,13 @@ class GridTest {
         assertThat(grid.cols).isEqualTo(5)
     }
     @Test
-    fun `must return all positions of the neighbours of the cell at the center of the grid`() {
-        val grid = Grid(3, 3)
+    fun `must return all positions of the neighbours at the center of the grid`() {
+      /*  val grid = Grid(3, 3)
         val cellNeighbourPositions = grid.getNeighbourPositions(Position(1, 1), 1)
         assertThat(cellNeighbourPositions).isEqualTo(setOf(
             Position(0, 0), Position(0, 1), Position(0, 2),
             Position(1, 0), Position(1, 2),
-            Position(2, 0), Position(2, 1), Position(2, 2)))
+            Position(2, 0), Position(2, 1), Position(2, 2)))*/
+        Assertions.fail("todo position")
     }
 }
