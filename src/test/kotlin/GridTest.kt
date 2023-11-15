7a9ea3efffc5f88e5fdc4ed0902bc5547d560bcd
alelarge <armelle.lelarge@shodo.io>
2023-11-15 22:53:37 +0100

    test passed positions of the neighbours at bottom

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -13,4 +13,18 @@ class GridTest {
             Position(1, 0), Position(1, 2),
             Position(2, 0), Position(2, 1), Position(2, 2)))
     }
+
+    @Test
+    fun `must return all positions of the neighbours at bottom right`() {
+        // Given
+        val grid = Grid(3, 3)
+        val cellNeighbourPositions = grid.getNeighbourPositions(Position(2, 2), 1)
+        // When
+        val expectedNeighbours = setOf(
+            Position(1, 1), Position(1, 2),
+            Position(2, 1)
+        )
+        // Then
+        Assertions.assertThat(expectedNeighbours.toSet()).isEqualTo(cellNeighbourPositions.toSet())
+    }
 }
