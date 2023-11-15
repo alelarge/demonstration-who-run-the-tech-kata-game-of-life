16b0072f82f33f436d4726daa165b07377dabfd4
alelarge <armelle.lelarge@shodo.io>
2023-11-15 21:46:26 +0100

    test failed no compile grid 3x5

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 class GridTest {
     @Test
     fun `initialized a 3x5 grid`(){
+        val grid =Grid(3,5)
         assertThat(grid.rows).isEqualTo(3)
         assertThat(grid.cols).isEqualTo(5)
     }
